6f722df9d929cea867c11c601827d07f8e3d14b7
jun.wu <jun.wu3@b-and-qchina.com>
2017-05-12 14:32:05 +0800

    update

--- learning/learning/learning-common/src/main/java/com/wujun/learning/commom/exceptions/CommonRuntimeException.java
+++ learning/learning/learning-common/src/main/java/com/wujun/learning/commom/exceptions/CommonRuntimeException.java
@@ -7,6 +7,7 @@ public class CommonRuntimeException extends RuntimeException {
 	 * Test git 1
 	 * Test git 2
 	 * Test git 3
+	 * Test git 4
 	 */
 	private static final long serialVersionUID = 5121475041850621312L;
 	
