fe40c8cbceeca4a73ac9636b7e6e5f9fa9481dab
jun.wu <jun.wu3@b-and-qchina.com>
2017-05-16 16:56:42 +0800

    update

--- learning/learning/learning-web-boot/src/test/java/com/wujun/learning/MyControllerTests.java
+++ learning/learning/learning-web-boot/src/test/java/com/wujun/learning/MyControllerTests.java
@@ -4,34 +4,41 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.wujun.learning.api.CommonController;
 import com.wujun.learning.config.ConfigInfo;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(CommonController.class)
+@SpringBootTest(classes = { LearningApplication.class })
 public class MyControllerTests {
-
-    @Autowired
-    private MockMvc mvc;
 
     @MockBean
     private ConfigInfo properties;
 
+    @Autowired
+    WebApplicationContext context;
+    
+    MockMvc mvc;
+    
+    @Before
+    public void setup(){
+    	mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    
     @Test
     public void testExample() throws Exception {
-//        given(this.userVehicleService.getVehicleDetails("sboot"))
-//                .willReturn(new VehicleDetails("Honda", "Civic"));
-        this.mvc.perform(get("/sboot/vehicle").accept(MediaType.TEXT_PLAIN))
-                .andExpect(status().isOk()).andExpect(content().string("Honda Civic"));
+        mvc.perform(get("/common/greet?name=wujun").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk()).andExpect(content().string("Hello wujun"));
     }
 
 }
